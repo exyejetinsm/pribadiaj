a8d6a5896e37f206193f5b45633104e183a17bda
falanzzz <lanzzxyz@hotmail.com>
2023-04-04 01:21:59 +0700

    fix session

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ const {
 const pribadi = "0b8e1aff";
 const ini_mark = `0@s.whatsapp.net`;
 const ownernya = ownernomer + "@s.whatsapp.net";
-
+let settingss = require("./key.json");
 //TIME
 const xtime = moment.tz("Asia/Jakarta").format("HH:mm:ss");
 const xdate = moment.tz("Asia/Kolkata").format("DD/MM/YYYY");
@@ -12628,32 +12628,44 @@ Judul: ${judul} `,
       // ADD FITUR
       case "ai":
       case "openai":
-        {
-          if (!m.isGroup) return m.reply(`Khusus Grup kak`);
+        try {
+          if (settingss.keyopenai === "ISI_APIKEY_OPENAI_DISINI")
+            return m.reply(
+              "Apikey belum diisi\n\nSilahkan isi terlebih dahulu apikeynya di file key.json\n\nApikeynya bisa dibuat di website: https://beta.openai.com/account/api-keys"
+            );
+          if (!text)
+            return m.reply(
+              `Chat dengan AI.\n\nContoh:\n${prefix}${command} Apa itu resesi`
+            );
           const configuration = new Configuration({
-            apiKey: `${global.apiAi}`,
+            apiKey: settingss.keyopenai,
           });
           const openai = new OpenAIApi(configuration);
-          const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: q,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0.2,
-            presence_penalty: 0,
+
+          /*const response = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: text,
+          temperature: 0, // Higher values means the model will take more risks.
+          max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+          top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+          frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+          presence_penalty: 0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      });
+        m.reply(`${response.data.choices[0].text}`);*/
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: text }],
           });
-
-          let image = {
-            url: "https://telegra.ph/file/796dacd1c968925696b46.jpg",
-          };
-          const teks = response.data.choices[0].text;
-
-          XeonBotInc.sendMessage(
-            from,
-            { image: image, caption: teks },
-            { quoted: m }
-          );
+          m.reply(`${response.data.choices[0].message.content}`);
+        } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("Maaf, sepertinya ada yang error :" + error.message);
+          }
         }
         break;
       default:
